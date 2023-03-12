bd77442c4c0f7d4c9b4041c594a82b50a115c003
irekkkarimov <irekkkarimov@mail.ru>
2023-03-12 17:28:29 +0300

    Refactored WordSet.cs, moved here stuff from the old LinkedList.

--- WordSetProject/WordSet.cs
+++ WordSetProject/WordSet.cs
@@ -3,25 +3,47 @@ namespace WordSetProject;
 
 public class WordSet
 {
-    public MyLinkedList Set;
+    public MyLinkedList<string> Set;
+    public readonly int? WordLength;
 
-    // Constructors
-    public WordSet()
+    // Constructor
+    public WordSet(string[] arr = null, int? length = null)
     {
-        Set = new MyLinkedList(Array.Empty<string>(), null);
-    }
-    public WordSet(string[] arr)
-    {
-        Set = new MyLinkedList(arr, null);
-    }
-    public WordSet(string[] arr, int length)
-    {
-        Set = new MyLinkedList(arr, length);
+        WordLength = length;
+        arr = arr != null ? SortArray(arr) : arr;
+        Set = new MyLinkedList<string>();
+        if (arr == null) {}
+        else
+        {
+            foreach (var word in arr)
+            {
+                ArgumentThrower(word);
+                if (Set.Head == null)
+                {
+                    Set.Head = new Node<string>(word);
+                    Set.Tail = Set.Head;
+                }
+                else if (Set.Tail.Value.CompareTo(word) != 0)
+                {
+                    Set.AddLast(word);
+                }
+            }
+        }
     }
 
-    public static WordSet NewWordSetByWordLength(int length)
+    public WordSet NewWordSetByWordLength(int length)
     {
-        return new WordSet(Array.Empty<string>(), length);
+        var result = new WordSet(null, length);
+        foreach (var item in Set)
+        {
+            var value = item.Value;
+            if (value.Length <= length)
+            {
+                result.Set.AddLast(value);
+            }
+        }
+
+        return result;
     }
 
     // Sorry for garbage code(
@@ -29,24 +51,25 @@ public class WordSet
     {
         var current1 = w1.Set.Head;
         var current2 = w2.Set.Head;
-        Set = new MyLinkedList(Array.Empty<string>(), null);
-
-
-        while (current1 == null && current2 == null)
+        Set = new MyLinkedList<string>();
+        
+        while (!(current1 == null && current2 == null))
         {
             if (current1 == null)
             {
                 Set.AddLast(current2.Value);
                 current2 = current2.Next;
+                continue;
             }
 
             if (current2 == null)
             {
                 Set.AddLast(current1.Value);
                 current1 = current1.Next;
+                continue;
             }
 
-            switch (current1.Value.CompareTo(current2))
+            switch (current1.Value.CompareTo(current2.Value))
             {
                 case 1:
                 {
@@ -78,9 +101,9 @@ public class WordSet
         int length = arr.Length;
         for (int i = 0; i < arr.Length; i++)
         {
-            for (int j = 0; j < arr.Length; j++)
+            for (int j = i; j < arr.Length; j++)
             {
-                if (arr[i].CompareTo(arr[j]) > 0)
+                if (arr[i].CompareTo(arr[j]) == 1)
                 {
                     (arr[i], arr[j]) = (arr[j], arr[i]);
                 }
@@ -92,46 +115,72 @@ public class WordSet
 
     public void Out(string filename)
     {
-        using (StreamWriter writer = new StreamWriter(filename))
+        using (StreamWriter writer = new StreamWriter($"C:\\Users\\Booba\\RiderProjects\\WordSetProject\\WordSetProject\\bin\\Debug\\net6.0\\{filename}"))
         {
-            var current = Set.Head;
-            while (current != null)
+            foreach (var node in Set)
             {
-                writer.WriteLine(current.Value);
-                current = current.Next;
+                writer.WriteLineAsync(node.Value);
             }
         }
     }
 
     public void Insert(string word)
     {
-        var current = Set.Head;
-        Node previous = null;
-        
-        while (true)
+        ArgumentThrower(word);
+        if (Set.IsEmpty())
         {
-            if (current == null)
+            Set.AddFirst(word);
+        }
+        if (Set.Tail.Value.CompareTo(word) < 0) Set.AddLast(word);
+        else
+        {
+            var current = Set.Head;
+            Node<string> previous = null;
+
+            while (current != null)
             {
-                Set.AddLast(word);
-                break;
-            }
-            if (word.CompareTo(current.Value) < 0)
-            {
-                if (current == Set.Head)
+                if (current == null) Set.AddLast(word);
+                if (word.CompareTo(current.Value) == 0) break;
+
+                if (word.CompareTo(current.Value) < 0)
                 {
-                    Set.AddFirst(word);
+                    if (current == Set.Head)
+                    {
+                        Set.AddFirst(word);
+                        break;
+                    }
+                    
+                    Set.AddAfter(previous, word);
                     break;
                 }
-                
-                Set.AddAfter(previous, word);
-                break;
-            }
 
-            previous = current;
-            current = current.Next;
+                previous = current;
+                current = current.Next;
+            }
         }
     }
 
+    public void Delete(string word)
+    {
+        Node<string> current = null;
+        foreach (var item in Set)
+        {
+            if (item.Value.CompareTo(word) == 0)
+            {
+                if (current == null)
+                {
+                    Set.RemoveFirst();
+                }
+                else
+                {
+                    Set.RemoveAfter(current);
+                }
+            }
+
+            current = item;
+        }
+    }
+    
     public WordSet[] VowelDivide()
     {
         // Russian and english vowels
@@ -157,7 +206,7 @@ public class WordSet
 
     public void RemovePalindrome()
     {
-        Node previous = null;
+        Node<string> previous = null;
 
         foreach (var node in Set)
         {
@@ -170,7 +219,7 @@ public class WordSet
         }
     }
 
-    public static bool IsPalindrome(string word)
+    private bool IsPalindrome(string word)
     {
         for (int i = 0; i < word.Length / 2; i++)
         {
@@ -178,5 +227,27 @@ public class WordSet
         }
 
         return true;
+    }
+    
+    public void ArgumentThrower(string word)
+    {
+        if (!IfContainsOnlyLetters(word)) throw new ArgumentException("The string argument was not a word");
+        if (WordLength != null && word.Length > WordLength)
+            throw new ArgumentException("Word is not of the appropriate length");
+    }
+    
+    public static bool IfContainsOnlyLetters(string word)
+    {
+        var result = false;
+        if (word.Length == 0) throw new ArgumentException();
+        foreach (var symbol in word)
+        {
+            if (Char.IsLetter(symbol))
+            {
+                result = true;
+            }
+        }
+
+        return result;
     }
 }
